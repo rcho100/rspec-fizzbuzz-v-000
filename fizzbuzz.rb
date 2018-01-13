5ffdb153d293e5d456463791f9c2e8833bfc3775
Richard Cho <richardcho100@gmail.com>
2018-01-13 07:04:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 
 
-def fizzbuzz(int)
-  if int % 3 == 0
+def fizzbuzz(number)
+  if (number % 3 == 0) && (number % 5 == 0)
     "Fizz"
-  elsif int % 5 == 0
+  elsif number % 5 == 0
     "Buzz"
-  elsif (int % 3 == 0) && (int % 5 == 0)
+  elsif number % 3 == 0
     "FizzBuzz"
   end
 end
