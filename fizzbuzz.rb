364b1dff4cf8560b2955cbb280372b3e7990b77d
Richard Cho <richardcho100@gmail.com>
2018-01-13 07:05:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 
 def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
+    "FizzBuzz"
+  elsif number % 3 == 0
     "Fizz"
   elsif number % 5 == 0
-    "Buzz"
-  elsif number % 3 == 0
     "FizzBuzz"
   end
 end
